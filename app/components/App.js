543b585081ba1cc26350b7bc1e88bc9952a489e0
Mark Tse <neverendingqs@users.noreply.github.com>
2017-11-30 15:45:53 -0500

    app - added ability to generate new GUIDs. (#8)

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,17 @@
 const React = require('react');
 const uuid = require('uuid/v4');
 
+const generateGuids = (n = 10) => ({ guids: Array(n).fill(0).map(() => uuid()).join('\n') });
+
 module.exports = React.createClass({
-  getInitialState: function() {
-    return { guids: Array(10).fill(0).map(i => uuid()).join('\n') }
-  },
+  getInitialState: generateGuids,
+  regenerateGuids: function() { this.setState(generateGuids()); },
 
   render: function () {
     return (
-      <div className="form-group row">
-        <pre>{this.state.guids}</pre>
+      <div>
+        <div className="form-group row"><pre>{this.state.guids}</pre></div>
+        <button onClick={this.regenerateGuids} type="button" className="btn btn-info">Waste More GUIDs!</button>
       </div>
     );
   }
