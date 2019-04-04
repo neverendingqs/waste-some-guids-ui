104c070415deedf96510e3972e6d28e761e03641
Mark Tse <neverendingqs@users.noreply.github.com>
2019-04-03 22:45:24 -0400

    app - adding buttons that allow copying the guids. (#9)

--- app/components/App.js
+++ app/components/App.js
@@ -1,17 +1,33 @@
 const React = require('react');
 const uuid = require('uuid/v4');
 
-const generateGuids = (n = 10) => ({ guids: Array(n).fill(0).map(() => uuid()).join('\n') });
+const generateGuids = (n = 10) => ({ guids: Array(n).fill(0).map(() => uuid()) });
 
 module.exports = React.createClass({
   getInitialState: generateGuids,
   regenerateGuids: function() { this.setState(generateGuids()); },
 
+  handleCopyToClipboard: function(e, guid) {
+    e.preventDefault();
+    const ref = this.refs[guid];
+    ref.focus();
+    ref.select();
+    document.execCommand('copy');
+  },
+
   render: function () {
     return (
       <div>
-        <div className="form-group row"><pre>{this.state.guids}</pre></div>
-        <button onClick={this.regenerateGuids} type="button" className="btn btn-info">Waste More GUIDs!</button>
+        <button onClick={this.regenerateGuids} type="button" className="btn btn-success">Waste More GUIDs!</button>
+        <hr />
+        {this.state.guids.map(guid => 
+          <div className="form-inline" key={guid}>
+            <input type="text" readOnly={true} className="form-control" style={{ margin: '2px', width: '300px' }} value={guid} ref={guid} />
+            <button className="btn btn-info" onClick={e => this.handleCopyToClipboard(e, guid)}>Copy</button>
+          </div>
+        )}
+        <hr />
+        <pre>{this.state.guids.join('\n')}</pre>
       </div>
     );
   }
